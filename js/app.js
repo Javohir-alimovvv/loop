be0f8d7462b6e8833c941a05cfe8cac49cdec857
javohir <ajavohir969@gmail.com>
2024-09-02 01:42:49 +0500

    masala

---
+++ js/app.js
@@ -0,0 +1,182 @@
+
+
+//  FOR
+
+// 1#
+// {
+//     let n = 10
+//     let k = 1
+
+//     for (let i = 0; i < n; i++) {
+//         console.log(k);
+//     }
+// }
+
+
+// 2#
+// {
+//     let a = 1;
+//     let b = 10;
+//     let length = 0
+//     for (let i = a; i <= b; i++) {
+//         console.log(i);
+//         length++
+//     }
+//     console.log(length:${length});
+// }
+
+
+
+// 3#
+
+// {
+//     let a = 10;
+//     let b = 0;
+//     let length = 0
+//     for (let i = a - 1; i > b; i--) {
+//         console.log(i);
+//         length++
+//     }
+//     console.log(length:${length});
+// }
+
+
+
+// 4#
+
+// {
+//     let kg = 5;
+
+//     for (let i = 1; i <= 10; i++) {
+//         let money = kg * i;
+//         console.log(${i} kg konfet narxi: ${money} so'm);
+//     }
+// }
+
+
+
+
+// 5#
+
+// {
+//     let kg = 5;
+
+//     for (let i = 0.1; i <= 1; i += 0.1) {
+//         let money = kg * i;
+//         console.log(${i.toFixed(1)} kg konfet narxi: ${money.toFixed(2)} so'm);
+//     }
+// }
+
+
+
+// 6#
+
+// {
+//     let kg = 5;
+
+//     for (let i = 1.2; i <= 2.1; i += 0.1) {
+//         let money = kg * i;
+//         console.log(${i.toFixed(1)} kg konfet narxi: ${money.toFixed(2)} so'm);
+//     }
+// }
+
+
+
+// 7#
+
+
+// {
+//     let a = 1
+//     let b = 10
+//     let c = 0
+
+//     for (let i = a; i <= b; i++) {
+//         c += i;
+//     }
+//     console.log(c);
+// }
+
+
+
+// 8#
+
+// {
+//     let a = 1
+//     let b = 5
+//     let c = 1
+
+//     for (let i = a; i <= b; i++) {
+//         c *= i;
+//     }
+//     console.log(c);
+// }
+
+
+
+// 9#
+
+// {
+//     let a = 5
+//     let b = 10
+//     let c = 0
+
+//     for (let i = a; i <= b; i++) {
+//         c += i * i;
+//     }
+//     console.log(c);
+// }
+
+
+// 10#
+
+// {
+//     let n = 2;
+//     let s = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//         s += 1 / i;
+//     }
+//     console.log(s);
+// }
+
+
+//WHILE
+
+
+// 1#
+// {
+//     let a = 54
+//     let b = 10
+//     while (a > b) {
+//         a -= b
+//     }
+//     console.log(a);
+// }
+
+
+
+// 2#
+// {
+//     let a = 9;
+//     let b = 2;
+//     let count = 0;
+//     while (a >= b) {
+//         a -= b
+//         count++
+//     }
+//     console.log(`${count}`);
+// }
+
+
+
+// 3#
+// {
+//     let n = 7
+//     let k = 2
+//     let count = 0
+//     while (n >= k) {
+//         n -= k
+//         count++
+//     }
+//     console.log(`butun son ${count} qoldiq son ${n}`);
+// }
